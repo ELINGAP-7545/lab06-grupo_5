e2298604bc32fe513740d972c1740cf445d4eca7
Luis Garcia <luisf.garciar@ecci.edu.co>
2020-05-14 18:16:45 -0500

    Update display.v

--- src/display.v
+++ src/display.v
@@ -1,15 +1,41 @@
 `timescale 1ns / 1ps
+
+
+
 module display(
-    input [15:0] num,
+
+    output [3:0] datOutRa,
+	 output [3:0] datOutRb,
     input clk,
     output [0:6] sseg,
-    output reg [3:0] an,
-	 input rst
+    output reg [1:0] an,
+	 input rst,
+	 
+	 input [3:0] addrRa,
+    input [3:0] addrRb,
+    
+	 input [3:0] addrW,
+    input [3:0] datW,
+    
+	 input RegWrite
     );
+	 
+	 
+	 BancoRegistro display ( 
+	 .datOutRa (datOutRa), 
+	 .datOutRb(datOutRb), 
+	 .addrRa (addrRa), 
+	 .addrRb (addrRb),
+	 .addrW (addrW),
+	 .datW (datW),	
+	 .RegWrite (RegWrite),
+	 .clk (clk),
+	 .rst (rst) 
+	 
+	);
+	 
 
-
-
-reg [3:0]bcd=0;
+reg [3:0]bcd = 0;
  
 BCDtoSSeg bcdtosseg(.BCD(bcd), .SSeg(sseg));
 
@@ -19,7 +45,8 @@ wire enable;
 // Divisor de frecuecia
 
 assign enable = cfreq[16];
-assign led =enable;
+assign led = enable;
+
 always @(posedge clk) begin
   if(rst==1) begin
 		cfreq <= 0;
@@ -28,19 +55,21 @@ always @(posedge clk) begin
 	end
 end
 
-reg [1:0] count =0;
+reg count = 0;
+
 always @(posedge enable) begin
+
 		if(rst==1) begin
-			count<= 0;
-			an<=4'b1111; 
-		end else begin 
+			count<= 0; 
+			an <= 2'b11;
+			end else begin 
 			count<= count+1;
-			an<=4'b1101; 
+			an<=2'b10; 
+
 			case (count) 
-				2'h0: begin bcd <= num[3:0];   an<=4'b1110; end 
-				2'h1: begin bcd <= num[7:4];   an<=4'b1101; end 
-				2'h2: begin bcd <= num[11:8];  an<=4'b1011; end 
-				2'h3: begin bcd <= num[15:12]; an<=4'b0111; end 
+			   2'h0: begin bcd <= datOutRa;   an<=2'b10; end 
+				2'h1: begin bcd <= datOutRb;   an<=2'b01; end 
+				
 			endcase
 		end
 end
