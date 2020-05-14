a4f8618b4f6b56deb65bcd76ebbaf7290fe2068e
Luis Garcia <luisf.garciar@ecci.edu.co>
2020-05-14 18:16:37 -0500

    Update BancoRegistro.v

--- src/BancoRegistro.v
+++ src/BancoRegistro.v
@@ -1,62 +1,77 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    09:55:28 10/12/2019 
-// Design Name: 	 ferney alberto beltran
-// Module Name:    BancoRegistro 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+
 module BancoRegistro #(      		 //   #( Parametros
-         parameter BIT_ADDR = 8,  //   BIT_ADDR Número de bit para la dirección
-         parameter BIT_DATO = 4,  //  BIT_DATO  Número de bit para el dato
-			parameter   RegFILE= "src/Reg16.men")
+         parameter BIT_ADDR = 4,  //   BIT_ADDR Número de bit para la dirección
+         parameter BIT_DATO = 2  //  BIT_DATO  Número de bit para el dato
+)
+
 	(
     input [BIT_ADDR-1:0] addrRa,
     input [BIT_ADDR-1:0] addrRb,
+	 
     
-	 output [BIT_DATO-1:0] datOutRa,
-    output [BIT_DATO-1:0] datOutRb,
+	 output [BIT_ADDR-1:0] datOutRa,
+    output [BIT_ADDR-1:0] datOutRb,
     
-	 input [BIT_ADDR:0] addrW,
-    input [BIT_DATO-1:0] datW,
+	 input [BIT_ADDR-1:0] addrW,
+    input [BIT_ADDR-1:0] datW,
     
 	 input RegWrite,
     input clk,
     input rst
     );
+	 
+	/* 
+	 display display ( 
+	 .datOutRa (datOutRa), 
+	 .datOutRb(datOutRb), 
+	 .addrRa (addrRa), 
+	 .addrRb (addrRb),
+	 .addrW (addrW),
+	 .datW (datW),	
+	 .RegWrite (RegWrite),
+	 .clk (clk),
+	 .rst (rst) 
+	 
+	);
+*/
 
 // La cantdiad de registros es igual a: 
-localparam NREG = 2 ** BIT_ADDR;
+localparam NREG = 2 ** BIT_ADDR;             //16
   
 //configiración del banco de registro 
-reg [BIT_DATO-1: 0] breg [NREG-1:0];
+reg [3: 0] breg [NREG-1:0];                  //[NREG-1:0] -> 15 registros de (reg [3: 0] breg) -> 4 bits cada registro 
 
 
-assign  datOutRa = breg[addrRa];
-assign  datOutRb = breg[addrRb];
+assign  datOutRa = breg[addrRa];					//salidaA = banco breg en la posicion addrRa
+assign  datOutRb = breg[addrRb];					//salidaB = banco breg en la posicion addrRb
+
+integer index;
 
 always @(posedge clk) begin
-	if (RegWrite == 1)
-     breg[addrW] <= datW;
-  end
 
+if (rst == 1)begin
+
+    for (index=0; index<16; index=index+1) begin
+      breg[index] <= 4'b1111;
+   
+	end
+	end
+
+	if (RegWrite == 1)
+     breg[addrW] <= datW;							//ESCRITURA EN LA POSICION addrW del dato datW
+  end
+												
 
 
 initial begin
-	$readmemh(RegFILE, breg);
+
+    for (index=0; index<16; index=index+1) begin
+      breg[index] <= 4'b1111;
+   
+	end
 end
+
 
 endmodule
 
